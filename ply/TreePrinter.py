928312ff6509264f43a06628f859efe897744478
Numernabis <luigi.zielone@gmail.com>
2019-04-08 11:49:03 +0200

    global space

--- ply/TreePrinter.py
+++ ply/TreePrinter.py
@@ -9,58 +9,51 @@ def addToClass(cls):
     return decorator
 
 class TreePrinter:
+    global space 
+    space = "|  "
 
     @addToClass(AST.Node)
     def printTree(self, indent=0):
         raise Exception("printTree not defined in class " + self.__class__.__name__)
 
     @addToClass(AST.IntNum)
-    def printTree(self, indent=0):
-        space = "|  "
+    def printTree(self, indent=0):   
         print(indent*space + str(self.value))
 
     @addToClass(AST.FloatNum)
-    def printTree(self, indent=0):
-        space = "|  "
+    def printTree(self, indent=0):       
         print(indent*space + str(self.value))
     
     @addToClass(AST.String)
-    def printTree(self, indent=0):
-        space = "|  "
+    def printTree(self, indent=0):        
         print(indent*space + self.value)
 
     @addToClass(AST.Id)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         print(indent*space + self.name)
 
     @addToClass(AST.UMinus)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         print(indent*space + "UMINUS")
         self.expr.printTree(indent + 1)
 
     @addToClass(AST.Transpose)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):       
         print(indent*space + "TRANSPOSE")
         self.expr.printTree(indent + 1)
 
     @addToClass(AST.Block)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         for expr in self.exprs:
             expr.printTree(indent)
 
     @addToClass(AST.MatrixSpecialMethod)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         print(indent*space + self.method)
         self.size.printTree(indent + 1)
 
     @addToClass(AST.Matrix)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):       
         print(indent*space + "VECTOR")
         for row in self.rows:
             print((indent+1)*space + "VECTOR")
@@ -68,22 +61,19 @@ class TreePrinter:
                  cell.printTree(indent+2)
 
     @addToClass(AST.BinExpr)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):      
         print(indent*space + str(self.op))
         self.left.printTree(indent + 1)
         self.right.printTree(indent + 1)
 
     @addToClass(AST.Assign)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):       
         print(indent*space + str(self.assignType))
         self.left.printTree(indent + 1)
         self.right.printTree(indent + 1)
 
     @addToClass(AST.For)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         print(indent*space + "FOR")
         self.id.printTree(indent + 1)
         print((indent+1)*space + "RANGE")
@@ -92,15 +82,13 @@ class TreePrinter:
         self.expr.printTree(indent + 1)
 
     @addToClass(AST.While)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):       
         print(indent*space + "WHILE")
         self.condition.printTree(indent + 1)
         self.ifTrue.printTree(indent + 1)
 
     @addToClass(AST.If)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         print(indent*space + "IF")
         self.condition.printTree(indent + 1)
         print(indent*space + "THEN")
@@ -110,35 +98,30 @@ class TreePrinter:
             self.ifFalse.printTree(indent + 1)
 
     @addToClass(AST.Print)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):        
         print(indent*space + "PRINT")
         #self.cells.printTree(indent + 1)
         for expr in self.cells:
             expr.printTree(indent + 1)
 
     @addToClass(AST.Return)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):      
         print(indent*space + "RETURN")
         self.expr.printTree(indent + 1)
 
     @addToClass(AST.Ref)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):       
         print(indent*space + "REF")
         self.id.printTree(indent + 1)
         self.firstIndex.printTree(indent + 1)
         self.secondIndex.printTree(indent + 1)
 
     @addToClass(AST.Break)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):       
         print(indent*space + self.name)
 
     @addToClass(AST.Continue)
-    def printTree(self,indent=0):
-        space = "|  "
+    def printTree(self,indent=0):      
         print(indent*space + self.name)
 
     @addToClass(AST.Error)
